3b6323e400089f2142d0e29b2b463fc66d379f85
frekkanzer2 <abatefrancesco98@gmail.com>
2019-10-17 17:24:42 +0200

    Update Proxy module

--- ClassesModules/Proxy.py
+++ ClassesModules/Proxy.py
@@ -1,6 +1,6 @@
 class Proxy:
-    def __init__(self, cls):
-        self._object = cls()
+    def __init__(self, cls, *args):
+        self._object = cls(*args)
 
     def __getattr__(self, attrName):
         return getattr(self._object, attrName, None)
